d34c6c8f2b43fb9087eb842693dda8309b08902e
Luigi Minardi <luigiminardim@gmail.com>
2021-07-29 22:18:48 -0300

    refactor: Change parameter name.

--- src/preset/manager.ts
+++ src/preset/manager.ts
@@ -18,7 +18,7 @@ addons.register(ADDON_ID, () => {
   // Register the panel
   addons.add(PANEL_ID, {
     type: types.PANEL,
-    title: "My addon",
+    title: "Global Controls",
     match: ({ viewMode }) => viewMode === "story",
     render: Panel,
   });
