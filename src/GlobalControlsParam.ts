0b3faf9de17c58ddec34c29084b0ec06058d2f56
Luigi Minardi <luigiminardim@gmail.com>
2021-07-30 18:47:32 -0300

    Set global parameters via controls.

---
+++ src/GlobalControlsParam.ts
@@ -0,0 +1,5 @@
+import { PresetColor } from "@storybook/components";
+
+export type GlobalControlsParam = {
+  presetColors?: PresetColor[];
+}
--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -1,10 +1,7 @@
-import { ADDON_ID } from './constants';
 import { StoryFn, StoryContext } from "@storybook/addons";
-import { ReactNode } from 'react';
-
 export const withGlobals =
   (decorated: (Story: any, values: Record<string, any>, context: StoryContext) => any) =>
     (story: any, context: StoryContext) => {
-      const values = context.globals[ADDON_ID]
+      const values = context.globals
       return decorated(story, values, context);
     }
