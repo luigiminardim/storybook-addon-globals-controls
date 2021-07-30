79a2b32977ed929ee499c0a7dbfbbbc7681e403b
Luigi Minardi <luigiminardim@gmail.com>
2021-07-30 18:46:45 -0300

    refactor: Remove unecessary constants.

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,3 @@
 export const ADDON_ID = "luigiminardim/global-controls";
 export const PANEL_ID = `${ADDON_ID}/panel`;
 export const PARAM_KEY = `globalControls`;
-
-export const EVENTS = {
-  RESULT: `${ADDON_ID}/result`,
-  REQUEST: `${ADDON_ID}/request`,
-  CLEAR: `${ADDON_ID}/clear`,
-};
