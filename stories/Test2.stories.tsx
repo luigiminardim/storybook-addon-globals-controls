48789b729269ee07cfc45e116202d2f6f5b949ec
Luigi Minardi <luigiminardim@gmail.com>
2021-07-30 19:04:06 -0300

    test: Improve stories so we can that the state of the globals whe the story changes.

--- stories/Test2.stories.tsx
+++ stories/Test2.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta } from "@storybook/react";
 
 export default {
-  title: "Json tester 1",
+  title: "Json tester 2",
 } as Meta;
 
 export function test() {
-  return "";
+  return "Assert that the change of story doesn't affect the globals.";
 }
